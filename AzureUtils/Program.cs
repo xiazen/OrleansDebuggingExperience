19f975229dc7cfccd4cd840590b9b8a7a9224190
xiazen <xiazen@microsoft.com>
2016-11-18 18:05:09 -0800

    init commit

---
+++ AzureUtils/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+
+namespace AzureUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+        }
+    }
+}
